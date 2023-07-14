476c136901d0bf12500d5314a5b1cd58c427db93
Panthr75 <jjireland@stu.naperville203.org>
2023-07-14 14:11:32 -0500

    initial commit

--- src/Discord.Net.Rest/API/Common/InstallParams.cs
+++ src/Discord.Net.Rest/API/Common/InstallParams.cs
@@ -1,18 +1,13 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Discord.API
+namespace Discord.API;
+
+internal class InstallParams
 {
-    internal class InstallParams
-    {
-        [JsonPropertyName("scopes")]
-        public string[] Scopes { get; set; } = Array.Empty<string>();
-        [JsonPropertyName("permissions")]
-        public ulong Permission { get; set; }
-    }
+    [JsonPropertyName("scopes")]
+    public string[] Scopes { get; set; } = Array.Empty<string>();
+    [JsonPropertyName("permissions")]
+    public ulong Permission { get; set; }
 }
