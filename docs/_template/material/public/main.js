d98fa4844b40c9c25116cb42057499ae55fe069b
FluffyDoggo <fluffydoggogithub@gmail.com>
2024-02-18 17:05:40 -0600

    Improve new docs site.

--- docs/_template/material/public/main.js
+++ docs/_template/material/public/main.js
@@ -4,6 +4,11 @@ export default {
             icon: 'github',
             href: 'https://github.com/discord-net/Discord.Net',
             title: 'GitHub'
+        },
+        {
+            icon: 'discord',
+            href: 'https://discord.gg/dnet',
+            title: 'Discord'
         }
     ]
 }
